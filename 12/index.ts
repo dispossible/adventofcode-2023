d899dea5ad635599e10db6b5348db067b28e8197
Timothy Bailey <mail@dispossible.com>
2023-12-14 17:47:21 +0000

    New validator for counter

--- 12/index.ts
+++ 12/index.ts
@@ -12,8 +12,11 @@ type Match = {
     text: string;
 };
 
+const start = Date.now();
+const runtime = () => Date.now() - start;
+
 (async () => {
-    const input = await readFile("12/test.txt");
+    const input = await readFile("12/input.txt");
 
     const rows: Input[] = input.map((line) => {
         const [towers, groups] = line.split(" ");
@@ -25,18 +28,19 @@ type Match = {
 
     const unfolded = rows.map(unfold);
 
-    const sequences = generateSequences(unfolded[6]);
-    // const sequences = unfolded.map((input, line) => {
-    //     console.log({
-    //         line,
-    //     });
-    //     return generateSequences(input);
-    // });
+    // const sequences = generateSequences(unfolded[8]);
+    const sequences = unfolded.map((input, line) => {
+        console.log({
+            line,
+        });
+        return generateSequences(input);
+    });
 
-    // console.log(sequences.join(","));
-    // console.log({
-    //     sum: sum(sequences),
-    // });
+    console.log(sequences.join(","));
+    console.log({
+        sum: sum(sequences),
+        runtime: runtime(),
+    });
 })();
 function findAllMatches(reg: RegExp, str: string) {
     reg = new RegExp(reg, "g");
@@ -62,7 +66,6 @@ function generateSequences(input: Input) {
 
     console.log(input);
 
-    const totalTowers = sum(input.groups);
     for (const [index, group] of input.groups.entries()) {
         const precedingSets = input.groups
             .slice(0, index)
@@ -80,84 +83,81 @@ function generateSequences(input: Input) {
         regex += followingSets ? `(?=[\\.\\?]+${followingSets}[\\.\\?]*$)` : `(?=[\\.\\?]*$)`;
 
         const matcher = new RegExp(regex);
-        console.log(matcher);
 
         matches.push(findAllMatches(matcher, input.towers));
     }
 
-    const sequences = new Set<string>();
+    let sequences = 0;
     const counters = matches.map((match) => ({
         state: 0,
         max: match.length - 1,
     }));
 
-    console.log(matches);
-
     let tick = 0;
-    let counterIndexToDecrement = counters.length - 1;
-    search: while (counterIndexToDecrement >= 0) {
-        // Generate the sequence
-        let sequence = input.towers;
-        const matchesToUse = counters.map((counter, index) => matches[index][counter.state]);
-        for (const match of matchesToUse) {
-            sequence = replaceAt(sequence, match.index, match.text.replace(/\?/g, "#"));
+    let ticking = true;
+    const decrementCounter = (counterIndex: number) => {
+        const counter = counters[counterIndex];
+        if (!counter) {
+            ticking = false;
+            return;
         }
-        sequence = sequence.replace(/\?/g, ".");
 
-        if (validate(sequence, input.groups)) {
-            sequences.add(sequence);
-
-            //if (tick % 100_000 === 0)
-            console.log(counters.map((counter) => counter.state).join("-"), sequence, "valid", tick);
+        if (counter.state >= counter.max) {
+            decrementCounter(counterIndex - 1);
+            const previousCounter = counters[counterIndex - 1];
+            if (!previousCounter) {
+                counter.state = 0;
+                return;
+            }
+            const previousMatches = matches[counterIndex - 1];
+            const previousMatch = previousMatches[previousCounter.state];
+            const resetPoint = matches[counterIndex].findIndex(
+                (m) => m.index > previousMatch.index + previousMatch.text.length
+            );
+            counter.state = resetPoint ?? 0;
         } else {
-            //if (tick % 100_000 === 0)
-            console.log(counters.map((counter) => counter.state).join("-"), sequence, "invalid", tick);
+            counter.state++;
         }
+    };
 
-        // Advance this dumb loop
-        counterIndexToDecrement = counters.length - 1;
-        tick: while (counterIndexToDecrement >= 0) {
-            const counterToDecrement = counters[counterIndexToDecrement];
-            if (counterToDecrement.state >= counterToDecrement.max) {
-                const previousCounter = counters[counterIndexToDecrement - 1];
-                if (previousCounter) {
-                    const previousMatches = matches[counterIndexToDecrement - 1];
-                    const previousIndex = previousMatches[previousCounter.state].index;
-                    const resetPoint = matches[counterIndexToDecrement].findIndex((m) => m.index > previousIndex);
-                    counterToDecrement.state = resetPoint ?? 0;
-                } else {
-                    counterToDecrement.state = 0;
-                }
-                counterIndexToDecrement--;
-            } else {
-                counterToDecrement.state++;
-                break tick;
+    const validateCounters = () => {
+        const hashes = /#+/g;
+        let found: RegExpExecArray;
+        while ((found = hashes.exec(input.towers))) {
+            const counterHasHash = counters.some((counter, i) => {
+                const match = matches[i][counter.state];
+                return found.index >= match.index && found.index < match.index + match.text.length;
+            });
+            if (!counterHasHash) {
+                return false;
             }
         }
+        return true;
+    };
+
+    while (ticking) {
+        if (tick % 1_000_000 === 0)
+            console.log(
+                counters.map((counter) => `${counter.state.toFixed(0).padStart(2, "0")}`).join(" - "),
+                tick,
+                runtime()
+            );
+
+        if (validateCounters()) {
+            sequences++;
+        }
+
+        decrementCounter(counters.length - 1);
         tick++;
     }
 
-    console.log(sequences.size);
-    return sequences.size;
-}
-
-function validate(towers: string, groups: number[]) {
-    if (towers.indexOf("?") >= 0) {
-        return false;
-    }
-    const sets = towers
-        .replace(/\.{2,}/g, ".")
-        .split(".")
-        .filter((s) => !!s);
-    const setCounts = sets.reduce((lengths: number[], set) => [...lengths, set.length], []);
-
-    const isValid = setCounts.every((count, i) => count === groups[i]) && setCounts.length === groups.length;
-    return isValid;
+    console.log({ permutations: sequences });
+    return sequences;
 }
 
 function unfold(input: Input): Input {
     // Part 1
-    return input;
+    // return input;
 
     // Part 2
     return {
