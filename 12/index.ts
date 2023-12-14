ebd9bc4f50968717271a913f92fe1fd14b52a33d
Timothy Bailey <mail@dispossible.com>
2023-12-14 19:58:14 +0000

    Skipped invalid chunks

--- 12/index.ts
+++ 12/index.ts
@@ -28,27 +28,29 @@ const runtime = () => Date.now() - start;
 
     const unfolded = rows.map(unfold);
 
-    // const sequences = generateSequences(unfolded[8]);
-    const sequences = unfolded.map((input, line) => {
-        console.log({
-            line,
-        });
-        return generateSequences(input);
-    });
+    // return generateSequences(unfolded[2], 2);
 
+    const sequences = unfolded.map((input, line) => {
+        return generateSequences(input, line);
+    });
     console.log(sequences.join(","));
     console.log({
         sum: sum(sequences),
         runtime: runtime(),
     });
 })();
+
 function findAllMatches(reg: RegExp, str: string) {
     reg = new RegExp(reg, "g");
     const matches: Match[] = [];
     let found: RegExpExecArray;
+    let finds = 0;
+    console.log("Matching...", { reg, str });
     while ((found = reg.exec(str))) {
         matches.push({ text: found[0], index: found.index });
         reg.lastIndex = found.index + 1;
+        finds++;
+        console.log("Matching...", { finds });
     }
     if (matches.length < 1) {
         console.error({
@@ -61,11 +63,12 @@ function findAllMatches(reg: RegExp, str: string) {
     return matches;
 }
 
-function generateSequences(input: Input) {
+function generateSequences(input: Input, lineNumber: number) {
+    lineNumber++;
+
+    console.log({ ...input, lineNumber });
+
     const matches: Match[][] = [];
-
-    console.log(input);
-
     for (const [index, group] of input.groups.entries()) {
         const precedingSets = input.groups
             .slice(0, index)
@@ -95,7 +98,18 @@ function generateSequences(input: Input) {
 
     let tick = 0;
     let ticking = true;
-    const decrementCounter = (counterIndex: number) => {
+
+    const visualizeSequence = () => {
+        // Generate the sequence
+        let sequence = input.towers;
+        const matchesToUse = counters.map((counter, index) => matches[index][counter.state]);
+        for (const match of matchesToUse) {
+            sequence = replaceAt(sequence, match.index, match.text.replace(/\?/g, "#"));
+        }
+        return sequence.replace(/\?/g, ".");
+    };
+
+    const incrementCounter = (counterIndex: number) => {
         const counter = counters[counterIndex];
         if (!counter) {
             ticking = false;
@@ -103,20 +117,79 @@ function generateSequences(input: Input) {
         }
 
         if (counter.state >= counter.max) {
-            decrementCounter(counterIndex - 1);
+            incrementCounter(counterIndex - 1);
             const previousCounter = counters[counterIndex - 1];
             if (!previousCounter) {
                 counter.state = 0;
                 return;
             }
-            const previousMatches = matches[counterIndex - 1];
-            const previousMatch = previousMatches[previousCounter.state];
+            const previousMatch = matches[counterIndex - 1][previousCounter.state];
             const resetPoint = matches[counterIndex].findIndex(
                 (m) => m.index > previousMatch.index + previousMatch.text.length
             );
             counter.state = resetPoint ?? 0;
         } else {
             counter.state++;
+        }
+    };
+
+    const validateCountersSkip = () => {
+        const hashes = /#+/g;
+        let found: RegExpExecArray;
+        while ((found = hashes.exec(input.towers))) {
+            const counterHasHash = counters.some((counter, i) => {
+                const match = matches[i][counter.state];
+                return found.index >= match.index && found.index < match.index + match.text.length;
+            });
+            if (!counterHasHash) {
+                //Is it before counter 0
+                if (matches[0][counters[0].state].index > found.index + found[0].length) {
+                    // Then we're done, nothing else will match
+                    counters.forEach((c) => (c.state = 9999));
+                }
+
+                // Ok so find the offender
+                const counterIBeforeHash = counters.findLastIndex(
+                    (counter, i) =>
+                        matches[i][counter.state].index + matches[i][counter.state].text.length < found.index
+                );
+                if (counterIBeforeHash < 0) {
+                    // :shrug:
+                    console.error("Failed to validate counters");
+                    return;
+                }
+
+                // Tick it on
+                incrementCounter(counterIBeforeHash);
+
+                // If that hasn't fixed it we need to reset the following counters
+                const counterBeforeHash = counters[counterIBeforeHash];
+                const matchBeforeHash = matches[counterIBeforeHash][counterBeforeHash.state];
+                if (matchBeforeHash.index + matchBeforeHash.text.length < found.index) {
+                    const counterAfterHash = counters[counterIBeforeHash + 1];
+                    // If there isn't a counter ahead, we need to jump to be valid
+                    if (!counterAfterHash) {
+                        const matchesBeforeHash = matches[counterIBeforeHash];
+                        const nextValidState = matchesBeforeHash.findIndex(
+                            (match) => match.index + match.text.length <= found.index
+                        );
+                        counterBeforeHash.state = nextValidState;
+                    } else {
+                        // Reset the following counters to their lowest positions that are valid
+                        for (let i = counterIBeforeHash + 1; i < counters.length; i++) {
+                            const counter = counters[i];
+                            const previousCounter = counters[i - 1];
+                            const previousMatch = matches[i - 1][previousCounter.state];
+                            const resetPoint = matches[i].findIndex(
+                                (m) => m.index > previousMatch.index + previousMatch.text.length
+                            );
+                            counter.state = resetPoint ?? 0;
+                        }
+                    }
+                }
+
+                validateCountersSkip();
+            }
         }
     };
 
@@ -135,19 +208,25 @@ function generateSequences(input: Input) {
         return true;
     };
 
+    validateCountersSkip();
     while (ticking) {
         if (tick % 1_000_000 === 0)
             console.log(
-                counters.map((counter) => `${counter.state.toFixed(0).padStart(2, "0")}`).join(" - "),
+                lineNumber,
+                counters.map((counter) => `${counter.state.toFixed(0).padStart(2, "0")}`).join("-"),
+                visualizeSequence(),
+                //validateCounters(),
                 tick,
                 runtime()
             );
 
-        if (validateCounters()) {
-            sequences++;
-        }
+        incrementCounter(counters.length - 1);
+        validateCountersSkip();
 
-        decrementCounter(counters.length - 1);
+        //if (validateCounters()) {
+        sequences++;
+        //}
+
         tick++;
     }
 
